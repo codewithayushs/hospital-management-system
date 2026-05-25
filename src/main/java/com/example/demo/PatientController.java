069afc104fdc2de73dfceef5fc98564a9603c113
codewithayushs <ayushshenwar09@gmail.com>
2026-05-25 11:31:15 +0530

    Added get patients API

--- src/main/java/com/example/demo/PatientController.java
+++ src/main/java/com/example/demo/PatientController.java
@@ -1,9 +1,16 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class PatientController {
 
     @Autowired
@@ -15,5 +22,11 @@ public class PatientController {
         repo.save(p);
 
         return "Patient Added Successfully";
+    }
+
+    @GetMapping("/patients")
+    public List<Patient> getPatients() {
+
+        return repo.findAll();
     }
 }
